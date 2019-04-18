04ed2cc81f63086e43f4184f41f53d44659f7ee4
Markus Kaarto <mkaarto@gmail.com>
2019-04-18 17:02:46 +0300

    maybe it works now?

--- app.js
+++ app.js
@@ -21,74 +21,75 @@ app.get('/', (req, res) => {
 	res.send(appEnv)
 })
 console.log(appEnv)
-// const tieraAssistant = appEnv.getServiceCreds('Tiera ChatBot')
+console.log(appEnv.getServiceCreds('Tiera ChatBot'))
+const tieraAssistant = appEnv.services.conversation[0].credentials
 
-// // Set up Assistant service wrapper.
-// var service = new AssistantV1({
-// 	iam_apikey: tieraAssistant.iam_apikey,
-// 	url: tieraAssistant.url,
-// 	version: tieraAssistant.version
-// })
+// Set up Assistant service wrapper.
+var service = new AssistantV1({
+	iam_apikey: tieraAssistant.iam_apikey,
+	url: tieraAssistant.url,
+	version: tieraAssistant.version
+})
 
-// var workspace_id = '96ceb624-7531-4103-b461-5570e9666869' // replace with workspace ID
+var workspace_id = '96ceb624-7531-4103-b461-5570e9666869' // replace with workspace ID
 
-// io.on('connection', function(socket) {
-// 	//send a message to watson and get the response and send the response to the client connected on this event.
+io.on('connection', function(socket) {
+	//send a message to watson and get the response and send the response to the client connected on this event.
 
-// 	service.message(
-// 		{
-// 			workspace_id: workspace_id
-// 		},
-// 		function(err, response) {
-// 			if (err) {
-// 				console.error(err) // something went wrong
-// 				socket.emit('disconnected', 'Bot Not available')
-// 			} else if (response.output.text.length != 0) {
-// 				socket.contextId = response.context
-// 				console.log(response.context.conversation_id)
-// 				socket.emit('server message', response.output.text)
-// 			}
-// 		}
-// 	)
+	service.message(
+		{
+			workspace_id: workspace_id
+		},
+		function(err, response) {
+			if (err) {
+				console.error(err) // something went wrong
+				socket.emit('disconnected', 'Bot Not available')
+			} else if (response.output.text.length != 0) {
+				socket.contextId = response.context
+				console.log(response.context.conversation_id)
+				socket.emit('server message', response.output.text)
+			}
+		}
+	)
 
-// 	socket.on('client message', function(message, cb) {
-// 		service.message(
-// 			{
-// 				workspace_id: workspace_id,
-// 				input: {text: message},
-// 				context: sanitizedContext(socket.contextId)
-// 			},
-// 			function(err, response) {
-// 				console.log('watson response ', JSON.stringify(response.output))
-// 				if (err) {
-// 					console.error(err) // something went wrong
-// 					return
-// 				}
+	socket.on('client message', function(message, cb) {
+		service.message(
+			{
+				workspace_id: workspace_id,
+				input: {text: message},
+				context: sanitizedContext(socket.contextId)
+			},
+			function(err, response) {
+				console.log('watson response ', JSON.stringify(response.output))
+				if (err) {
+					console.error(err) // something went wrong
+					return
+				}
 
-// 				// If an intent was detected, log it out to the console.
-// 				if (response.intents.length > 0) {
-// 					console.log('Detected intent: #' + response.intents)
-// 				}
+				// If an intent was detected, log it out to the console.
+				if (response.intents.length > 0) {
+					console.log('Detected intent: #' + response.intents)
+				}
 
-// 				// Display the output from dialog, if any.
-// 				if (response.output.length != 0) {
-// 					socket.contextId = response.context
-// 					cb(response.output)
-// 				}
-// 			}
-// 		)
-// 	})
-// })
-// function sanitizedContext(context) {
-// 	// function to remove all context variables from context, there might not be a context object so we'll try it first!
-// 	try {
-// 		const newContext = context
-// 		delete newContext.buttons
-// 		delete newContext.link
-// 		delete newContext.camera
-// 		delete newContext.command
-// 		return newContext
-// 	} catch (e) {
-// 		return {}
-// 	}
-// }
+				// Display the output from dialog, if any.
+				if (response.output.length != 0) {
+					socket.contextId = response.context
+					cb(response.output)
+				}
+			}
+		)
+	})
+})
+function sanitizedContext(context) {
+	// function to remove all context variables from context, there might not be a context object so we'll try it first!
+	try {
+		const newContext = context
+		delete newContext.buttons
+		delete newContext.link
+		delete newContext.camera
+		delete newContext.command
+		return newContext
+	} catch (e) {
+		return {}
+	}
+}
