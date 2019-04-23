4669a6f183da630eb99943ff40db928fd5989e66
mkaarto <mkaarto@gmail.com>
2019-04-23 15:11:39 +0300

    Pipeline tests (#1)
    fixes pipeline environment variable fetching
    get port number from environment in production (IBM seems to default node instances to 8080)
    some JS style changes: prefer const, use let over var

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 // Example 2: adds user input and detects intents.
-var express = require('express')
-var http = require('http')
-var app = express()
-var server = http.createServer(app).listen(3000)
-var io = require('socket.io')(server)
-var AssistantV1 = require('watson-developer-cloud/assistant/v1')
-var cfenv = require('cfenv')
-//var dotenv = require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app).listen(process.env.PORT || 4000)
+const io = require('socket.io')(server)
+const AssistantV1 = require('watson-developer-cloud/assistant/v1')
+const cfenv = require('cfenv')
 
-/* Here we setup the cloudant stuff to work both locally and on bluemix */
-// load local VCAP configuration  and service credentials
-var vcapLocal
+let vcapLocal
 try {
 	vcapLocal = require('./vcap-local.json')
 	console.log('Loaded local VCAP', vcapLocal)
 } catch (e) {}
+
 const appEnvOpts = vcapLocal ? {vcap: vcapLocal} : {}
 const appEnv = cfenv.getAppEnv(appEnvOpts)
-console.log(appEnv.getServiceCreds('TieraAssistant'))
-
-const tieraAssistant = appEnv.getServiceCreds('TieraAssistant')
+const tieraAssistant = appEnv.getServiceCreds('Tiera ChatBot')
 
 // Set up Assistant service wrapper.
-var service = new AssistantV1({
-	iam_apikey: tieraAssistant.iam_apikey,
+const service = new AssistantV1({
+	iam_apikey: tieraAssistant.apikey,
 	url: tieraAssistant.url,
-	version: tieraAssistant.version
+	version: "2019-04-16"
 })
 
-var workspace_id = '96ceb624-7531-4103-b461-5570e9666869' // replace with workspace ID
+const workspace_id = '96ceb624-7531-4103-b461-5570e9666869' // replace with workspace ID
 
 io.on('connection', function(socket) {
-	//send a message to watson and get the response and send the response to the client connected on this event.
-
+	// send a empty message to watson and get the response and send the 
+	// response to the client connected on this event.
+	
 	service.message(
 		{
-			workspace_id: workspace_id
+			workspace_id: workspace_id,
+			input: {text: ''}
 		},
 		function(err, response) {
 			if (err) {
@@ -43,8 +41,7 @@ io.on('connection', function(socket) {
 				socket.emit('disconnected', 'Bot Not available')
 			} else if (response.output.text.length != 0) {
 				socket.contextId = response.context
-				console.log(response.context.conversation_id)
-				socket.emit('server message', response.output.text)
+				socket.emit('server message', response.output)
 			}
 		}
 	)
@@ -77,6 +74,7 @@ io.on('connection', function(socket) {
 		)
 	})
 })
+
 function sanitizedContext(context) {
 	// function to remove all context variables from context, there might not be a context object so we'll try it first!
 	try {
